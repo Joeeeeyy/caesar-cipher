9ba534f83aa97ebced791c2e5d2d0921a96fa4ee
Jose Guillermo Lopez <joseglopez1289@gmail.com>
2022-11-07 12:36:09 -0800

    figured out a second solution

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,14 +3,32 @@
 # > caesar_cipher("What a string!", 5)
 # => "Bmfy f xywnsl!"
 
+# def caesar_cipher(string, shift)
+#    alphabet = Array('a'..'z')
+#    non_caps = alphabet.zip(alphabet.rotate(shift)).to_h
+#    alphabet = Array('A'..'Z')
+#    caps = alphabet.zip(alphabet.rotate(shift)).to_h
+#    encrypter = non_caps.merge(caps)
+#    string.chars.map { |char| encrypter.fetch(char, char) }.join
+#  end
+
+
+# puts caesar_cipher("What a string!", 5)
+
 def caesar_cipher(string, shift)
-   alphabet = Array('a'..'z')
-   non_caps = alphabet.zip(alphabet.rotate(shift)).to_h
-   alphabet = Array('A'..'Z')
-   caps = alphabet.zip(alphabet.rotate(shift)).to_h
-   encrypter = non_caps.merge(caps)
-   string.chars.map { |char| encrypter.fetch(char, char) }.join
- end
+  input = string.split('')
+  encrypter = input.map do |character|
+    if character == "z"
+      ('a'.ord + shift - 1).chr
+    elsif character == "Z"
+      ('A'.ord + shift - 1).chr
+    elsif character.upcase != character.downcase
+      (character.ord + shift).chr
+    else
+      character
+    end
+  end
+  p encrypter.join('')
+end
 
-
-puts caesar_cipher("What a string!", 5)
+caesar_cipher('What a string!', 5)
